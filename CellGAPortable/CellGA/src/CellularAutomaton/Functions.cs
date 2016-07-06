6bdb16fbf43c01966d4cf84e0cec761815380a67
kurema <kurema_makoto_software@yahoo.co.jp>
2016-07-07 03:45:59 +0900

    Ported to dotnet core.

---
+++ CellGAPortable/CellGA/src/CellularAutomaton/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CellGA
+{
+    public static class Functions
+    {
+        public static double Distance(params double[] crd)
+        {
+            double sum = 0;
+            for (int i = 0; i < crd.Count(); i += 2)
+            {
+                sum += Math.Pow(crd[i] - crd[i + 1], 2);
+            }
+            return Math.Sqrt(sum);
+        }
+    }
+}
---
+++ CellGAPortable/CellGA/src/TestConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CellGA.RhinoTools.CellObject co = new CellGA.RhinoTools.CellObject();
+
+            Console.WriteLine("Hello, world!");
+            Console.Read();
+        }
+    }
+}
